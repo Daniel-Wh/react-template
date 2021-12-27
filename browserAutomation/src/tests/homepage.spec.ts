1300975da68010853fdd9b707102cc8a77b74f9e
Daniel Whitney <whitneyd@southwestern.edu>
2021-12-27 14:31:28 -0600

    breaking to test

--- browserAutomation/src/tests/homepage.spec.ts
+++ browserAutomation/src/tests/homepage.spec.ts
@@ -3,5 +3,5 @@ import { test, expect} from '@playwright/test';
 test('Confirm text on homepage', async ({ page }) => {
     await page.goto('https://daniel-wh.github.io/react-template/')
 
-    await expect(page.locator(".App-link")).toHaveText("Learn React", {useInnerText: true})
+    await expect(page.locator(".App-link")).toHaveText("Lear React", {useInnerText: true})
 })
